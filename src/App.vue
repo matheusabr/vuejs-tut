9369334f2ac58fdfb9d34def31b5da2cbd94f1aa
matheusabreu <matheusabreu.dev@gmail.com>
2018-04-12 11:46:23 -0300

    chore(skills): add a simple component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <Skills/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Skills from './components/Skills'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Skills
   }
 }
 </script>
@@ -23,6 +22,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
